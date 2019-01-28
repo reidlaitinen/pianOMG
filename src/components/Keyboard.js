b4875e88188db4df573bb1a1cf10fc81f0c2c3ed
Reid Laitinen <reidlaitinen@gmail.com>
2019-01-28 15:20:00 -0700

    added some stuff

---
+++ src/components/Keyboard.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Keyboard extends React.Component {
+  render() {
+    return (
+      <div>Keyboard Component</div>
+    )
+  }
+
+}
+
+export default Keyboard;
