f3b2b6e869581757039df6124eaf94e3aed7870c
Reid Laitinen <reidlaitinen@gmail.com>
2019-02-05 08:17:48 -0700

    pull in Piano component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
+import Keyboard from './components/Keyboard.js';
 import {Grid} from 'semantic-ui-react';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Grid>
+      <Grid columns={3}>
         <Grid.Row>
           <div className="App">
             <h1>pianOMG</h1>
           </div>
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column>
+
+          </Grid.Column>
+          <Grid.Column>
+            <Keyboard />
+          </Grid.Column>
+          <Grid.Column>
+            
+          </Grid.Column>
         </Grid.Row>
       </Grid>
     );
--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -3,9 +3,20 @@ import { Piano, KeyboardShortcuts, MidiNumbers } from 'react-piano';
 import 'react-piano/dist/styles.css';
 
 class Keyboard extends React.Component {
+
   render() {
+    const firstNote = MidiNumbers.fromNote('c3');
+    const lastNote = MidiNumbers.fromNote('f5');
+    const keyboardShortcuts = KeyboardShortcuts.create({
+      firstNote: firstNote,
+      lastNote: lastNote,
+      keyboardConfig: KeyboardShortcuts.HOME_ROW,
+    })
     return (
-      <div>Keyboard Component</div>
+      <div>
+        <div>Keyboard Component</div>
+          <Piano noteRange={{ first: firstNote, last: lastNote }} />
+        </div>
     )
   }
 
