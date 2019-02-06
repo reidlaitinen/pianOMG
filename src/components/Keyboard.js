d300f51e5c6e3a29014229e53a7cd1aef590b898
Reid Laitinen <reidlaitinen@gmail.com>
2019-02-06 16:12:50 -0700

    working toward playability

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 import { Piano, KeyboardShortcuts, MidiNumbers } from 'react-piano';
 import 'react-piano/dist/styles.css';
 
+
+
 class Keyboard extends React.Component {
 
   render() {
-    const firstNote = MidiNumbers.fromNote('c3');
+    const firstNote = MidiNumbers.fromNote('a2');
     const lastNote = MidiNumbers.fromNote('f5');
     const keyboardShortcuts = KeyboardShortcuts.create({
       firstNote: firstNote,
@@ -14,9 +16,8 @@ class Keyboard extends React.Component {
     })
     return (
       <div>
-        <div>Keyboard Component</div>
-          <Piano noteRange={{ first: firstNote, last: lastNote }} width={1000} />
-        </div>
+        <Piano noteRange={{ first: firstNote, last: lastNote }} width={1000} />
+      </div>
     )
   }
 
