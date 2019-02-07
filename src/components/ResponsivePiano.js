fb0127d5d039f331750b9ea68f8d14246031cbf5
Reid Laitinen <reidlaitinen@gmail.com>
2019-02-07 14:27:08 -0700

    some fixes, and added react-dimensions

--- src/components/ResponsivePiano.js
+++ src/components/ResponsivePiano.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Piano, KeyboardShortcuts, MidiNumbers } from 'react-piano';
 import Soundfont from 'soundfont-player';
+import SoundfontProvider from '../SoundFontProvider.js';
+import DimensionsProvider from '../DimensionsProvider.js';
 
 import 'react-piano/dist/styles.css';
 
@@ -8,6 +10,15 @@ import 'react-piano/dist/styles.css';
 class ResponsivePiano extends React.Component {
 
   render() {
+
+    const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    const soundfontHostname = 'https://d1pzp51pvbm36p.cloudfront.net';
+    const noteRange = {
+      first: MidiNumbers.fromNote('a2'),
+      last: MidiNumbers.fromNote('f4'),
+    };
+    const props = this.props;
+
     return (
       <DimensionsProvider>
         {({ containerWidth, containerHeight }) => (
